aeb1333c865b01559183365fd48248a4c5323685
Tk <gamr.bio@gmail.com>
2026-05-22 09:23:46 -0300

    rota json

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API ONLINE 🚀')
-})
+app.get("/", (req, res) => {
+  res.send("API rodando 🚀");
+});
+
+app.get("/api/teste", (req, res) => {
+  res.json({
+    status: "online",
+    mensagem: "Servidor funcionando",
+    deploy: "Coolify",
+  });
+});
 
 app.listen(3000, () => {
-  console.log('Servidor rodando')
-})
+  console.log("Servidor rodando");
+});
